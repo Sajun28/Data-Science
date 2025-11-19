5871876d4787f56655fc4cd35e1b6f2d59423a7d
sachin <sachinsamuel287@gmail.com>
2025-11-19 19:46:00 +0530

    Data science import game commit

---
+++ Guessrandomno.py
@@ -0,0 +1,18 @@
+import random
+
+# Computer picks a random number between 1 and 100
+number = random.randint(1, 100)
+
+print("Welcome to the Number Guessing Game!")
+print("I have selected a number between 1 and 100.")
+
+while True:
+    guess = int(input("Enter your guess: "))
+
+    if guess < number:
+        print("Too low! Try again.")
+    elif guess > number:
+        print("Too high! Try again.")
+    else:
+        print("🎉 Correct! You guessed the number!")
+        break
